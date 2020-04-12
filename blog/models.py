58943d9237ecc0bb608aeac7fe96d2624d833737
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-04-12 12:11:43 +0530

    removed: function call from models.CASCADE

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.utils import  timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE())
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -23,7 +23,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE())
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
