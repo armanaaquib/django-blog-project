e64fd3fa9d716ebca667b6a38cbd1948f600d6cc
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-04-14 12:25:14 +0530

    Filtering: posts according to published_date

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.utils import timezone
 from .models import Post
 
 # Create your views here.
@@ -10,6 +11,10 @@ class PostListView(ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'post_list.html'
+
+    def get_queryset(self):
+        published_posts = Post.objects.filter(published_date__lte=timezone.now())
+        return published_posts.order_by('-published_date')
 
 class PostDetailView(DetailView):
     model = Post
