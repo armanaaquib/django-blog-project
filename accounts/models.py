d34db657957964c170fa51d899d5434ae061fb7d
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-04-15 08:38:19 +0530

    Added: UserProfile model in accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
+class UserProfile(auth.models.User):
+    portfolio_site = models.URLField(blank=True)
+    profile_pic = models.ImageField(blank=True)
+
+    def __str__(self):
+        return f'@{self.username}'
