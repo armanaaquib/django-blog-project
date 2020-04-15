13d8e213305c9cd3c44a93958a85426f9baf3632
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-04-15 09:11:21 +0530

    Added: TestProfileUser

---
+++ accounts/tests/test_model.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from accounts.models import UserProfile
+# Create your tests here.
+
+class TestUserProfile(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user_profile = UserProfile.objects.create_user(
+            first_name='John',
+            last_name ='Ram',
+            username='khan',
+            password='jrk1',
+            portfolio_site='https://www.google.com',
+        )
+        user_profile.save()
+
+    def test___str__(self):
+        user_profile = UserProfile.objects.get(username='khan')
+        
+        self.assertEqual(user_profile.__str__(), '@khan')
