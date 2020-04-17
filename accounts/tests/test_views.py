b6238f63c4c13c8f8d690becc7639dd7cf38edb8
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-04-17 11:44:31 +0530

    Added: LoginView

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from accounts.models import UserProfile
 from accounts.views import SignUpView
 
 class TestSignUpView(TestCase):
@@ -17,6 +18,16 @@ class TestSignUpView(TestCase):
         response = self.client.get(reverse('accounts:signUp'))
 
         self.assertTemplateUsed(response,'signUp.html')
+
+    def test_user_created(self):
+        form_data = {
+            'username':'khan',
+            'password':'jrk1',
+            'confirm_password':'jrk1',
+        }
+        self.client.post(reverse('accounts:signUp'), data=form_data)
+
+        self.assertTrue(UserProfile.objects.get(username='khan'))
 
     def test_view_success_url(self):
         form_data = {
@@ -43,3 +54,51 @@ class TestSignUpView(TestCase):
             field='password', 
             errors=['This password is too short. It must contain at least 4 characters.',]
         )
+
+class TestLoginView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        UserProfile.objects.create_user(
+            username='khan',
+            password='jk-r',
+        )
+    
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/accounts/login/')
+        
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessable_by_name(self):
+        response = self.client.get(reverse('accounts:login'))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('accounts:login'))
+
+        self.assertTemplateUsed(response,'login.html')
+
+    def test_is_authenticated(self):
+        form_data = {
+            'username':'khan',
+            'password':'jk-r',
+        }
+        response = self.client.post(reverse('accounts:login'), data=form_data, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated)
+
+    def test_is_not_authenticated(self):
+        form_data = {
+            'username':'khan',
+            'password':'jk-s',
+        }
+        response = self.client.post(reverse('accounts:login'), data=form_data, follow=True)
+        self.assertFalse(response.context['user'].is_authenticated)
+
+    def test_view_success_url(self):
+        form_data = {
+            'username':'khan',
+            'password':'jk-r',
+        }
+        response = self.client.post(reverse('accounts:login'), data=form_data)
+        
+        self.assertRedirects(response, reverse('blog:post-list'))
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.edit import FormView
+from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, LoginForm
+from .models import UserProfile
 # Create your views here.
 
 class SignUpView(FormView):
@@ -12,4 +14,18 @@ class SignUpView(FormView):
         userProfile = form.save()
         userProfile.set_password(userProfile.password)
         userProfile.save()
+        return super().form_valid(form)
+
+class LoginView(FormView):
+    form_class = LoginForm
+    template_name = 'login.html'
+    success_url = reverse_lazy('blog:post-list')
+
+    def form_valid(self, form):
+        username = self.request.POST['username']
+        password = self.request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+
         return super().form_valid(form)
